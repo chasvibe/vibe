b2a29cae414918708923a75ce7933fa4bef5e740
chasvibe <viktor.berg@chasacademy.se>
2021-10-06 10:34:33 +0200

    test commit again

--- test.py
+++ test.py
@@ -6,7 +6,14 @@ def my_second_function(surname, last_name):
     return f"Hello {surname} {last_name}!"
 
 
-def my_math_function(num1, num2, num3):
-    return num1 * (num2 / num3)
+def my_math_function(num1, num2):
+    return num1 * num2
 
 
+while True:
+    try:
+        numb1 = eval(input("Please enter a number: "))
+        numb2 = eval(input("Please enter one more: "))
+        print(my_math_function(numb1, numb2))
+    except TypeError:
+        print("Please enter an integer: ")
